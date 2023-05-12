dda200928f11514e32cfe8d67996e854c10b6b64
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-05-12 10:32:05 -0300

    Inserir Genero e Livro

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <string>
 
 using namespace std;
@@ -20,14 +19,13 @@ struct Editora
 struct Autor
 {
     int id;
-    char descricao[30];
+    char nome[30];
 };
 
-struct Data
+struct Genero
 {
-    int dia;
-    int mes;
-    int ano;
+    int id;
+    char descricao[40];
 };
 
 struct Livro
@@ -39,8 +37,14 @@ struct Livro
     int id_genero;
     int id_pessoa_emprestado;
     int quantidade_emprestada;
-    Data data_ultimo_emprestimo;
+    struct Data
+    {
+        int dia;
+        int mes;
+        int ano;
+    } data_ultimo_emprestimo;
 };
+
 
 
 /*
@@ -49,18 +53,55 @@ struct Livro
 void inserirPessoa(Pessoa p[], int &index, int quantidade);
 void inserirEditora(Editora e[], int &index, int quantidade);
 void inserirAutor(Autor a[], int &index, int quantidade);
+void inserirGenero(Genero g[], int &index, int quantidade);
+void inserirLivro(Livro l[], int &index, int quantidade);
 
 
 
+void lerData(int &dia, int &mes, int &ano){
+    cout << "\nInforme o dia: ";
+    cin >> dia;
+    cout << "Informe o mes: ";
+    cin >> mes;
+    cout << "Informe o ano: ";
+    cin >> ano;
+}
+
+void imprimirData(int &dia, int &mes, int &ano)
+{
+
+    if(dia < 10){
+        cout << 0 << dia << "/";
+    }else{
+        cout << dia << "/";
+    }
+    if (mes < 10){
+        cout << 0 << mes << "/";
+    }else {
+        cout << mes << "/";
+    }
+    cout << ano << endl;
+}
 
 int main()
 {
 
     const int QUANTIDADE = 1000;
 
-    Pessoa pessoas[QUANTIDADE]; int indexPessoas;
-    Editora editoras[QUANTIDADE]; int indexEditoras;
-    Autor autores[QUANTIDADE]; int indexAutores;
+    Pessoa pessoas[QUANTIDADE];
+    int indexPessoas;
+    
+    Editora editoras[QUANTIDADE];
+    int indexEditoras;
+
+    Autor autores[QUANTIDADE];
+    int indexAutores;
+
+    Genero generos[QUANTIDADE];
+    int indexGeneros;
+    
+    Livro livros[QUANTIDADE];
+    int indexLivros;
 
     char op = 'x';
     while (op != '0')
@@ -72,6 +113,8 @@ int main()
         cout << "[1] - [Adicionar Pessoas]\n";
         cout << "[2] - [Adicionar Editoras]\n";
         cout << "[3] - [Adicionar Autores]\n";
+        cout << "[4] - [Adicionar Generos]\n";
+        cout << "[5] - [Adicionar Livros]\n";
 
         fflush(stdin);
         cout << "\n\nInforme a sua escolha: ";
@@ -82,7 +125,7 @@ int main()
         {
         case '0':
         {
-            cout << "Saindo...";
+            cout << "Saindo...\n";
             break;
         }
         case '1':
@@ -100,6 +143,16 @@ int main()
             inserirAutor(autores, indexAutores, QUANTIDADE);
             break;
         }
+        case '4':
+        {
+            inserirGenero(generos, indexGeneros, QUANTIDADE);
+            break;
+        }
+        case '5':
+        {
+            inserirLivro(livros, indexLivros, QUANTIDADE);
+            break;
+        }
         default:
         {
             cout << "Opcao invalida";
@@ -108,7 +161,6 @@ int main()
         }
         system("pause");
     }
-
 }
 
 void inserirPessoa(Pessoa p[], int &index, int quantidade)
@@ -118,12 +170,15 @@ void inserirPessoa(Pessoa p[], int &index, int quantidade)
     int i = 0;
     for (int saida = 1; i < quantidade && saida != 0; i++)
     {
-        cout << "\n\nId: "; cin >> p[i].id;
+        cout << "\n\nId: ";
+        cin >> p[i].id;
         fflush(stdin);
         if (p[i].id > 0)
         {
-            cout << "Nome: "; gets(p[i].nome);
-            cout << "Endereco: "; gets(p[i].endereco);
+            cout << "Nome: ";
+            gets(p[i].nome);
+            cout << "Endereco: ";
+            gets(p[i].endereco);
         }
         else
             saida = 0;
@@ -137,11 +192,13 @@ void inserirEditora(Editora e[], int &index, int quantidade)
     int i = 0;
     for (int saida = 1; i < quantidade && saida != 0; i++)
     {
-        cout << "\n\nId: "; cin >> e[i].id;
+        cout << "\n\nId: ";
+        cin >> e[i].id;
         fflush(stdin);
         if (e[i].id > 0)
         {
-            cout << "Nome: "; gets(e[i].nome);
+            cout << "Nome: ";
+            gets(e[i].nome);
         }
         else
             saida = 0;
@@ -155,14 +212,71 @@ void inserirAutor(Autor a[], int &index, int quantidade)
     int i = 0;
     for (int saida = 1; i < quantidade && saida != 0; i++)
     {
-        cout << "\n\nId: "; cin >> a[i].id;
+        cout << "\n\nId: ";
+        cin >> a[i].id;
         fflush(stdin);
         if (a[i].id > 0)
         {
-            cout << "Descricao: "; gets(a[i].descricao);
+            cout << "Nome: ";
+            gets(a[i].nome);
         }
         else
             saida = 0;
     }
     index = i - 1;
 }
+
+void inserirGenero(Genero g[], int &index, int quantidade)
+{
+    cout << "Inserindo Genero\n";
+    int i = 0;
+    for (int saida = 1; i < quantidade && saida != 0; i++)
+    {
+        cout << "\n\nId: ";
+        cin >> g[i].id;
+        fflush(stdin);
+        if (g[i].id > 0)
+        {
+            cout << "Nome: ";
+            gets(g[i].descricao);
+        }
+        else
+            saida = 0;
+    }
+    index = i - 1;
+}
+
+void inserirLivro(Livro l[], int &index, int quantidade)
+{
+    cout << "Inserindo Livros\n";
+    int i = 0;
+    for (int saida = 1; i < quantidade && saida != 0; i++)
+    {
+        cout << "\n\nId: ";
+        cin >> l[i].id;
+        fflush(stdin);
+        if (l[i].id > 0)
+        {
+            cout << "Nome: ";
+            gets(l[i].nome);
+            cout << "Id Editora: ";
+            cin >> l[i].id_editora;
+            cout << "Id Autor: ";
+            cin >> l[i].id_autor;
+            cout << "Id Genero: ";
+            cin >> l[i].id_genero;
+            cout << "Id Pessoa emprestado: ";
+            cin >> l[i].id_pessoa_emprestado;
+            cout << "Quantidade de vezes emprestado: ";
+            cin >> l[i].quantidade_emprestada;
+
+            cout << "Data do ultimo emprestimo";
+            lerData(l[i].data_ultimo_emprestimo.dia, l[i].data_ultimo_emprestimo.mes, l[i].data_ultimo_emprestimo.ano);
+            imprimirData(l[i].data_ultimo_emprestimo.dia, l[i].data_ultimo_emprestimo.mes, l[i].data_ultimo_emprestimo.ano);
+        }
+        else
+            saida = 0;
+    }
+    index = i - 1;
+}
+
